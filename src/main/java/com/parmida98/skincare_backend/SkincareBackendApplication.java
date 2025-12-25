60db4b0c5c8b803151cf669b054c0b363305ddf9
Parmida98 <Parmida.ahmadi@stud.sti.se>
2025-12-25 21:35:23 +0100

    Initial Commit

---
+++ src/main/java/com/parmida98/skincare_backend/SkincareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.parmida98.skincare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkincareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkincareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parmida98/skincare_backend/SkincareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parmida98.skincare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkincareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
